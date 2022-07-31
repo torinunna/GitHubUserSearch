91e2006502810b99360bce77c591b29145a5cebb
torinunna <torinunna9588@gmail.com>
2022-07-31 23:28:06 +0900

    implement searchController

--- GithubUserSearch/View/SearchViewController.swift
+++ GithubUserSearch/View/SearchViewController.swift
@@ -14,5 +14,31 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        embedSearchControl()
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "torinunna"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
     }
 }
